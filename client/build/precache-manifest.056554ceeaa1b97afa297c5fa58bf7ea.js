c34bff9abf225fa26f09a5c1e80484d9da9b533f
Karolyn Seredick <kseredick95@gmail.com>
2020-07-21 13:15:17 -0400

    create build file, config. finished server

---
+++ client/build/precache-manifest.056554ceeaa1b97afa297c5fa58bf7ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f51fe03731096c9881f91d8b917bca9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "902fe84ba9e31a3a146e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9110ea9efe2bbc6bb8dc",
+    "url": "/static/js/2.0c2ea7cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0c2ea7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "902fe84ba9e31a3a146e",
+    "url": "/static/js/main.4d1d18cd.chunk.js"
+  },
+  {
+    "revision": "1f492bc0475da8ea8b4a",
+    "url": "/static/js/runtime-main.8fa08245.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
