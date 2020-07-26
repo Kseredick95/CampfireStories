f848eb8682c7a37a95f18bb6ba3bab03e0dd31d4
Andrew Reeves <andrew.reeves.coding.bootcamp@gmail.com>
2020-07-26 15:22:49 -0400

    no real changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import RenderReg from "./pages/Register";
+import RenderStoryPage from "./pages/StoryPage"
 
 function App() {
   return (
     <Router>
       <div>
-        <Switch>
+        {/* <Switch>
           <Route exact path={["/", "/register"]}>
             <RenderReg />
+          </Route>
+        </Switch> */}
+
+        <Switch>
+          <Route exact path={["/", "/storypage"]}>
+            <RenderStoryPage />
           </Route>
         </Switch>
       </div>
