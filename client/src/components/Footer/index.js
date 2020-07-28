d86ede14651c75f5450a79319ecb2310c9d76b3b
Terry C <terrycetoute@gmail.com>
2020-07-28 09:24:44 -0400

    Footer completed

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.less";
+
+function Footer() {
+    return (
+        <footer class="site-footer">
+            {/* <hr></hr> */}
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by
+                        <a href="#">Campfire Stories</a>. </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
