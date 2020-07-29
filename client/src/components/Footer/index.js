f08aa748f498f263760e0a609e225220c8d26c8c
Terry C <terrycetoute@gmail.com>
2020-07-29 11:45:59 -0400

    refactored footer js using the grid js

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,17 +1,18 @@
 import React from "react";
+import { Container, Row, Col } from "../Grid";
 import "./style.css";
 
 function Footer() {
     return (
         <footer className="site-footer">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-12">
+            <Container fluid>
+                <Row>
+                    <Col size="12">
                         <p className="copyright-text">Copyright &copy; 2020 All Rights Reserved by
-                        <a href="#">Campfire Stories</a>. </p>
-                    </div>
-                </div>
-            </div>
+                        <a href="/">Campfire Stories</a>. </p>
+                    </Col>
+                </Row>
+            </Container>
         </footer>
     );
 }
