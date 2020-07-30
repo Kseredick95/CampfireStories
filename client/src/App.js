5d4c9431b238bf30c2bb806f089011f2a1c49e24
Terry C <terrycetoute@gmail.com>
2020-07-30 01:28:32 -0400

    modified and refactored the app js file

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import RenderReg from "./pages/Register";
 import RenderStoryPage from "./pages/StoryPage"
+import Navbar from "./components/Navbar";
+import Profile from "./pages/Profile";
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <Router>
       <div>
+        <Navbar />
         <Switch>
-          <Route exact path={["/", "/register"]}>
-            <RenderReg />
-          </Route>
+          <Route exact path="/" component={RenderReg} />
+          <Route exact path="/register" component={RenderReg} />
+          <Route exact path="/profile" component={Profile} />
+          {/* <Route exact path="/storypage" component={RenderStoryPage} /> */}
         </Switch>
-
-        {/* <Switch>
-          <Route exact path={["/", "/storypage"]}>
-            <RenderStoryPage />
-          </Route>
-        </Switch> */}
+        <Footer />
       </div>
     </Router>
   );
