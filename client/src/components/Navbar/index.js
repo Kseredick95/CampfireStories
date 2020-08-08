cd110161b9e46e6f04cf76138f51fc967da2ee29
Karolyn Seredick <kseredick95@gmail.com>
2020-08-07 20:33:44 -0400

    Update index.js
    re-added index.js

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 import store from "store"
+//Comment
 
 function handleLogout() {
     store.remove(`user`)
