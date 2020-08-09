fc47aa5c47eee470b170f4c3e71d6538e23b1223
Your Name <danygrimaldo@gmail.com>
2020-08-08 22:19:33 -0400

    updated book pic

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+// import { Provider as AlertProvider } from "react-alert";
+// import AlertTemplate from "react-alert-template-basic";
+import * as serviceWorker from "./serviceWorker";
+
+////////////////////////////////////////////////////
+//trial and error for alerts
+
+// const options = {
+//   position: "bottom center",
+//   timeout: 5000,
+//   offset: "30px",
+//   transition: "scale",
+// };
+
+// ReactDOM.render(
+//   <AlertProvider template={AlertTemplate} {...options}>
+//     < />
+//   </AlertProvider>,
+//   document.getElementById("root")
+// );
+
+////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////
+
+//orignal content
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+/////////////////////////////////////////////////
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
