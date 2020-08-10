979d17309a70af6aaa6b4bf562fbd0dde76ba220
Karolyn Seredick <kseredick95@gmail.com>
2020-08-10 14:07:33 -0400

    made aboutus page

--- client/src/components/Library/BookCard/BookCard.js
+++ client/src/components/Library/BookCard/BookCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row } from "../../Grid";
+import { Container } from "../../Grid";
 import {Link} from "react-router-dom"
 import BookPic from "../BookPic/BookPic"
 
