8e46b4042e96e0d4b10db7337f90b3a0107c6ac2
Karolyn Seredick <kseredick95@gmail.com>
2020-08-11 17:52:14 -0400

    fixed small bugs

--- client/src/components/NavbarTwo/index.js
+++ client/src/components/NavbarTwo/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import store from "store";
 
-const NavbarTwo = () => {
+function NavbarTwo(){
+    let user = store.get("user")
     return (
         <div id="nav-body-two">
             <nav className="navbar navbar-inverse navbar-expand-lg">
                 <div id="logoNavTwo">
-                    <Link className="navbar-brand" to="/">
+                    <Link className="navbar-brand" to={handleRedirect(user)}>
                         <img src={process.env.PUBLIC_URL + '/images/CampfireLogo.png'} alt="Campfire Stories Logo" loading="lazy" />
                     </Link>
                 </div>
             </nav>
         </div>
-    );
+    )
 };
 
+function handleRedirect(user) {
+    if (user === undefined) {
+        return ("/")
+    } else {
+        return ("/profile/" + user._id)
+    }
+}
 export default NavbarTwo;
