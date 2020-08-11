fd79bd2b895b916e037748b78e759c12f1f81aec
Andrew Reeves <andrew.reeves.coding.bootcamp@gmail.com>
2020-08-11 16:55:39 -0400

    Merge branch 'master' of https://github.com/Kseredick95/CampfireStories into on-submit
    catching up to master

--- client/src/components/NavbarTwo/index.js
+++ client/src/components/NavbarTwo/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import store from "store";
 
-const NavbarTwo = () => {
+function NavbarTwo(props){
+    
+    let user = store.get("user")
+ 
     return (
         <div id="nav-body-two">
             <nav className="navbar navbar-inverse navbar-expand-lg">
                 <div id="logoNavTwo">
-                    <Link className="navbar-brand" to="/">
+                    <Link className="navbar-brand" to={`/profile/${user._id}`}>
                         <img src={process.env.PUBLIC_URL + '/images/CampfireLogo.png'} alt="Campfire Stories Logo" loading="lazy" />
                     </Link>
                 </div>
             </nav>
         </div>
-    );
+    )
 };
 
 export default NavbarTwo;
