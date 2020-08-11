7e2d0b9568ed7a930b6ceeaff06e43a739126188
Your Name <danygrimaldo@gmail.com>
2020-08-10 20:43:40 -0400

    Fixed dashboard rendering errors along with library restart button styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import LibraryContainer from "./components/Library/LibraryContainer/LibraryContainer"
+import LibraryContainer from "./components/Library/LibraryContainer/LibraryContainer";
 import StoryPage from "./components/StoryPage/Container/Container";
 import Profile from "./pages/Profile";
-import Footer from "./components/Footer"
-import Aboutus from "./pages/Aboutus"
+import Footer from "./components/Footer";
+import Aboutus from "./pages/Aboutus";
 
 function App() {
-
   return (
     <Router>
       <div>
@@ -20,7 +19,7 @@ function App() {
           <Route exact path="/library" component={LibraryContainer} />
           <Route exact path="/storypage/:bookTitle" component={StoryPage} />
           <Route exact path="/profile/:_id" component={Profile} />
-          <Route exact path= "/about" component={Aboutus} />
+          <Route exact path="/about" component={Aboutus} />
         </Switch>
         <Footer />
       </div>
